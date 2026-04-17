84b1243957459da81996721a246e357196800ff2
thedustycube <nsmprm@gmail.com>
2026-04-17 17:05:32 +0600

    feat: complete TV show and season progress synchronization engine

--- app/src/main/java/com/dustycube/cinelog/data/local/WatchlistDao.kt
+++ app/src/main/java/com/dustycube/cinelog/data/local/WatchlistDao.kt
@@ -4,19 +4,15 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Upsert
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface WatchlistDao {
-    @Query("SELECT * " +
-            "FROM watchlist " +
-            "ORDER BY watchStatusOrder DESC")
+    @Query("SELECT * FROM watchlist ORDER BY watchStatusOrder DESC")
     fun getFullWatchlist(): Flow<List<WatchlistItemEntity>>
 
-    @Query("SELECT * " +
-            "FROM watchlist " +
-            "WHERE watchstatus IN ('WATCHING', 'REWATCHING') " +
-            "ORDER BY lastUpdatedTimeStamp DESC")
+    @Query("SELECT * FROM watchlist WHERE watchstatus IN ('WATCHING', 'REWATCHING') ORDER BY lastUpdatedTimeStamp DESC")
     fun getHomeWatchlist(): Flow<List<WatchlistItemEntity>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
@@ -28,8 +24,12 @@ interface WatchlistDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun upsertSeasonProgress(progress: SeasonProgressEntity)
 
-    @Query("SELECT * " +
-        "FROM season_progress " +
-        "WHERE showId = :showId")
+    @Query("SELECT * FROM season_progress WHERE showId = :showId")
     fun getSeasonProgress(showId: Int): Flow<List<SeasonProgressEntity>>
+
+    @Query("DELETE FROM season_progress WHERE showId = :showId AND seasonNumber = :seasonNumber")
+    suspend fun deleteSeasonProgress(showId: Int, seasonNumber: Int)
+
+    @Upsert
+    suspend fun upsertAllSeasonProgress(list: List<SeasonProgressEntity>)
 }
